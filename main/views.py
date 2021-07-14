5ad9ed3ed9d3091d6a9de52c1f7bf95dca7929c4
jimyihendrix <jimyihendric@gmail.com>
2021-07-14 09:48:49 +0200

    Changed main messagge on localhost

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<h1>Hi Vinter Team! I'm dying for learning from you</h1>")
+    return HttpResponse("<h1>Vinter Capital</h1>")
 
 def view1(response):
     return HttpResponse("<h2>Here goes my app</h2>")
