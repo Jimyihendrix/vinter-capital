0c40f5ab46d422effb93e5bfddbfbdf23f4883ca
Jimy <36779900+Jimyihendrix@users.noreply.github.com>
2021-07-14 12:02:22 +0200

    Update views.py
    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<h1>Vinter Capital</h1>")
+    return HttpResponse("<h1>Vinter Capital - vinter.co</h1>")
 
 def view1(response):
     return HttpResponse("<h2>Here goes my app</h2>")
