e3158008eb356bd9af6faa5a0e26abff21e59c2b
jimyihendrix <jimyihendric@gmail.com>
2021-07-14 12:36:38 +0200

    rest_framework settings added

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,17 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 
+# rest_framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+                'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
