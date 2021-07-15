aa0ac911df6cb711ffc489a8ce5517bce944e046
jimyihendrix <jimyihendric@gmail.com>
2021-07-15 10:56:23 +0200

    added username and password request.POST

--- config/views.py
+++ config/views.py
@@ -5,14 +5,32 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
+# imported auth hasher
+from django.contrib.auth.hashers import check_password
+# response
+from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['POST'])
 def login(request):
-    user = User.objects.get(username='admin')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response("Usuario invalido")
+
+    pwd_valid = check_password(password, user.password)
+
+    if not pwd_valid:
+        return Response("Contraseña Invalida")
 
     token = Token.objects.create(user=user)
 
     print(token.key)
+    return Response(token.key)
 
 
 
