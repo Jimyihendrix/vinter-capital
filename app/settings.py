9f6e43c8ff03d02f7d5c651ddbac89e32d033527
jimyihendrix <jimyihendric@gmail.com>
2021-07-16 13:21:21 +0200

    added user and password with token Authorization: Token ........

--- app/settings.py
+++ app/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # <-- for Token Auth Api
+    ],
 }
 
 
